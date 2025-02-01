1258d529504241871c531fbb5ace27c5dcb4dde8
gpombotky <gpombotky@gmail.com>
2025-02-01 14:53:04 -0300

    Create 1467.c

---
+++ 1467.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    
+    while (scanf("%d %d %d", &a, &b, &c) != EOF) {
+        if (a == b && a == c) {
+            printf("*\n");
+        } else if (a != b && a != c) {
+            printf("A\n");
+        } else if (b != a && b != c) {
+            printf("B\n");
+        } else {
+            printf("C\n");
+        }
+    }
+    
+    return 0;
+}
